1df2db25b2f82a738a50c2b70fe24c288a8a7ab7
Enrique Espinosa <quiqueenri2016@gmail.com>
2023-11-15 23:44:47 -0300

    favicon,card,carrusel,quique

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Arranca from './components/Arranca/arranca';
 import Productos from './components/Productos/Productos';
 import Footer from './components/Footer/Footer';
 import  Nosotros  from './components/Nosotros/Nosotros';
+import Carrusel from './components/Carrusel/Carrusel';
+
+
 
 
 
@@ -15,6 +18,7 @@ function App() {
       <Home/>
       <Arranca/>
       <Productos/>
+      <Carrusel/>
       <Nosotros/>
       <Footer/> 
       </>
